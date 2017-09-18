6931ce26643e80a0197d639ce359b00f0c41078b
dnjstm123 <pwm3753@gmail.com>
2017-09-18 17:09:20 +0900

    들여쓰기 수정

--- sample/src/sample/Test.java
+++ sample/src/sample/Test.java
@@ -2,7 +2,7 @@ package sample;
 
 public class Test {
 
-		public static void main(String[] args) {
-			System.out.println("hello world");
-		}
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
 }
