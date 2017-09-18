cfe64eed61cdf23471528ec3e839a398310c75b2
dnjstm123 <pwm3753@gmail.com>
2017-09-18 17:12:07 +0900

    구구단 TEST

--- sample/src/sample/Test.java
+++ sample/src/sample/Test.java
@@ -4,5 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		for(int i = 2 ; i < 10 ; i++) {
+			for(int j = 1 ; j < 10 ; j++) {
+				System.out.println(i+" * "+j +" = "+i*j  );
+			}
+		}
 	}
 }
